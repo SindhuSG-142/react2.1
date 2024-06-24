b39bab41f22b40a8be7437c949c898917330b51d
SindhuSG-142 <sgsindhusrinivas@gmail.com>
2024-06-24 19:14:17 +0530

    react 1 assignment

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+// src/components/Greeting.jsx
+
+import React from "react";
+
+function Greeting({ hello }) {
+  return (
+    <div>
+      <h2>{/* complete the missing code */}</h2>
+    </div>
+  );
+}
+
+export default Greeting;
